1eaf375193bddcd83529d23b8b67a7b90937ab4b
Luke Evans <levans002@gmail.com>
2012-08-31 12:21:18 -0600

    Checkpoint
    - Peer
    - PeerList
    
    Signed-off-by: Luke Evans <levans002@gmail.com>

--- src/utilities/Tools.java
+++ src/utilities/Tools.java
@@ -1,6 +1,51 @@
 package utilities;
 
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+
 // Functions used all over the program. Handy location
 public class Tools {
+
+	// ================================================================================
+	// Message functions
+	// ================================================================================
+	// Get Message type
+	public static int getMessageType(byte[] bytes) {
+		byte[] copy = bytes.clone();
+		ByteBuffer bbuff = ByteBuffer.wrap(copy);
+
+		// Size
+		bbuff.getInt();
+
+		// Return type
+		return bbuff.getInt();
+	}
+
+	// Generate random number
+	public static int generateRandomNumber() {
+
+		int Min = 0;
+		int Max = 65535;
+
+		int random = Min + (int) (Math.random() * ((Max - Min) + 1));
+
+		random -= 32768;
+
+		return random;
+	}
+
+	// ================================================================================
+	// Host Functions
+	// ================================================================================
+	public static String getLocalHostname() {
+		try {
+			String localhostname = java.net.InetAddress.getLocalHost().getHostName();
+			return localhostname;
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
